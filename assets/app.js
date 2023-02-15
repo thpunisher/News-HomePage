780310e29ed706148cda0fd2f3e2c8ac7534566d
Anass Koutit <anasskoutit2002@gmail.com>
2023-02-15 11:18:54 +0100

    js

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+const menuHamburger = document.querySelector(".burger")
+const navLinks = document.querySelector(".links-burger")
+ 
+menuHamburger.addEventListener('click',()=>{
+        document.getElementsById('links-burger').style.display = ""
+        })
